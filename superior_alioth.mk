dd1e8e46519ecb15dd578546d8a5e134f2e6db28
levi3609 <nazmulhasan3609@gmail.com>
2023-04-05 11:36:53 +0000

    Alioth: Bringup for Superior

--- superior_alioth.mk
+++ superior_alioth.mk
@@ -9,13 +9,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := superior_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -23,13 +23,15 @@ PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Rom Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
+# Boot resolution
+TARGET_BOOT_ANIMATION_RES := 1080
 
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := johnmart19
-CUSTOM_BUILD_TYPE := OFFICIAL
+# Supported Device Flags
+TARGET_SUPPORTS_NOW_PLAYING := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+#SuperioeOS Stuff
+SUPERIOR_OFFICIAL := false
+BUILD_WITH_GAPPS := true
+USE_QUICKPIC := false
+TARGET_GAPPS_ARCH := arm64
